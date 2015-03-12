61aa45ef45b4e3034e2a9041a299fc2f28ca7db5
Gladkikh Prohor <prohor33@gmail.com>
2015-03-12 22:51:59 +0300

    back to debug configuration

--- app/src/main/java/crystal/tech/gimmecollage/app/Settings.java
+++ app/src/main/java/crystal/tech/gimmecollage/app/Settings.java
@@ -4,7 +4,7 @@ package crystal.tech.gimmecollage.app;
  * Created by prohor on 31/12/14.
  */
 public class Settings {
-    static boolean isReleaseVersion = true;
+    static boolean isReleaseVersion = false;
     static boolean showAds = false; // no ads for a while
     static boolean collectStatistics = isReleaseVersion;
 }
